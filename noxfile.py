ad5428739e5dc0b7aaa26276e13e17696f5622fd
Joel Berendzen <joel@generisbio.com>
2021-02-27 17:52:14 -0700

    nox

--- noxfile.py
+++ noxfile.py
@@ -1,48 +1,26 @@
 # -*- coding: utf-8 -*-
-"""Nox testing environment defs."""
-# cribbed from https://cjolowicz.github.io/posts/hypermodern-python-03-linting/
-
-# standard library imports
-import tempfile
+"""Nox sessions."""
 
 # third-party imports
 import nox
+from nox_poetry import session
 
 CODE_LOCATIONS = (
     "pytest_datadir_mgr",
     "tests",
-    "noxfile.py",
 )
 
-
-def install_with_constraints(session, *args, **kwargs):
-    """Get dependencies from poetry."""
-    with tempfile.NamedTemporaryFile() as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--without-hashes",
-            "--dev",
-            "--format=requirements.txt",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        session.install(f"--constraint={requirements.name}", *args, **kwargs)
-
-
-@nox.session(python=["3.8"])
+@session
 def tests(session):
     """Run tests with pytest and pytest-cov."""
-    args = session.posargs or []
-    session.run("poetry", "install", "--no-dev", external=True)
-    install_with_constraints(session, "coverage[toml]", "pytest", "pytest-cov")
+    args = session.posargs or ["--cov=./", "--cov-report=xml"]
+    session.install("coverage[toml]", "pytest", "pytest-cov", ".")
     session.run("pytest", *args)
 
 
-@nox.session(python=["3.8"])
+@session
 def lint_pylint(session):
     """Run lint on all code."""
     args = session.posargs or CODE_LOCATIONS
-    session.run("poetry", "install", "--no-dev", external=True)
-    install_with_constraints(session, "pylint", "nox")
+    session.install("pylint", ".")
     session.run("pylint", *args)
